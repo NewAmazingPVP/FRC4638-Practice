15e4e97855d2aa043eddca339ef8f090608efbf8
NewAmazingPVP <87737759+NewAmazingPVP@users.noreply.github.com>
2023-01-21 14:25:52 -0500

    Initial commit

---
+++ src/main/java/frc/robot/commands/MotorSpeedAdjust.java
@@ -0,0 +1,42 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.commands;
+
+import edu.wpi.first.wpilibj.XboxController;
+import edu.wpi.first.wpilibj2.command.CommandBase;
+import edu.wpi.first.wpilibj2.command.Subsystem;
+import frc.robot.Robot;
+import frc.robot.subsystems.OneMotorSubsystem;
+
+public class MotorSpeedAdjust extends CommandBase {
+  private OneMotorSubsystem m_oneMotorSubsystem;
+  public double speed = 0.0;
+  /** Creates a new MotorSpeedAdjust. */
+  public MotorSpeedAdjust(OneMotorSubsystem subsystem, double spd) {
+    addRequirements(subsystem);
+    m_oneMotorSubsystem = subsystem;
+  }
+
+  // Called when the command is initially scheduled.
+  @Override
+  public void initialize() {}
+
+  // Called every time the scheduler runs while the command is scheduled.
+  @Override
+  public void execute() {
+    //System.out.println("Setting speed low");
+    m_oneMotorSubsystem.setPower(speed);
+  // Called once the command ends or is interrupted.
+  }
+
+  @Override
+  public void end(boolean interrupted) {}
+
+  // Returns true when the command should end.
+  @Override
+  public boolean isFinished() {
+    return false;
+  }
+}
